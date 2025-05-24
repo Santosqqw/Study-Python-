791943de56df8c0ba18a07357a87c2a0afb6bd0a
Anderson Silva <anderson09silva94@gmail.com>
2025-05-24 13:37:37 -0300

    Finalizacao do app

--- Sabor Express/app.py
+++ Sabor Express/app.py
@@ -2,6 +2,11 @@ import os
 restaurantes = [{'nome' : 'Praça', 'categoria' : 'Japonesa', 'ativo' : False},{'nome' : 'Pizza Suprema', 'categoria' : 'Pizza', 'ativo' : True}, {'nome' : 'Cantina', 'categoria' : 'Italiano', 'ativo' : False}]
 
 def exibir_nome_programa():
+    '''Essa função mostra o nome do nosso programa
+    
+    Output:
+    - Mostra o nome do valor inserido no print
+    '''
     print('Sabor Express\n')
 
 def exibir_subtitulo(texto):
@@ -13,28 +18,55 @@ def exibir_subtitulo(texto):
     print()
 
 def exibir_opcoes():
+    '''Essa função tem como base listar todas as opções do nosso App
+    
+    Output:
+    - Mostra a primeira opção que é á "Cadastrar restaurantes"
+    - Mostra a segunda opção que é á "Listar restaurantes"
+    - Mostra a terceira opção que é á "Alternar estado resturante"
+    - Mostra a quarta opção que é á "Sair"
+
+    '''
     print('1. Cadastrar restaurante')
     print('2. Listar restaurantes')
     print('3. Alternar estado restaurante')
     print('4. Sair\n')
 
 def finalizar_app():
+   '''Essa função tem como objetivo exibir uma mensagem assim que a opção finalizar app for selecionada'''
    exibir_subtitulo('Finalizando app')
 
 def opcao_invalida():
+    '''Essa função mostra quando um valor que foi inserido não faz parte das opçoes disponível e retorna o usuário para a o menu pricnipal'''
     print('Opção Inválida!!\n')
     menu_principal()
 
 def cadastrar_novo_restaurante():
+    ''' Essa função é responsalve por cadastrar um novo restaurante
+    
+    Inputs:
+    - Nome do restaurante
+    - Categoria 
+
+    Output:
+    - Adiciona um novo restaureante da lista de restaurantes 
+    '''
     exibir_subtitulo('Cadastros de novos restaurante')
     nome_do_restarante = input('Digite o nome do restaurante a ser cadastrado: ')
+    nome_do_restarante_minusculo = nome_do_restarante.lower()
     categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restarante}: ')
-    dados_do_restaurante = {'nome': nome_do_restarante, 'categoria': categoria, 'ativo': False}
+    dados_do_restaurante = {'nome': nome_do_restarante_minusculo, 'categoria': categoria, 'ativo': False}
     restaurantes.append(dados_do_restaurante)
     print(f'O restaurante {nome_do_restarante} foi cadastrado com sucesso!!')
     menu_principal()
 
 def listar_restaurantes():
+    '''Essa funcção é responsável por listar todos os restaurantes cadastrados
+    
+    Output:
+    - Exibir a quantidade de restaurantes cadastrados 
+    - Exibir os restaurantes cadastrados seguido por sua categoria e seu status de Ativo/Desativado
+    '''
     exibir_subtitulo('Lista de restaurantes cadastrados')
     quantidade_de_cadastros = len(restaurantes)
     print(f'Atualmente temos {quantidade_de_cadastros} cadastrados em nosso sistema\n')
@@ -49,16 +81,28 @@ def listar_restaurantes():
     menu_principal()
 
 def menu_principal():
+    '''Essa função tem com objetivo retornar o usuário ao menu principal'''
     input('\nDigite alguma tecla para voltar ao menu principal ')
     main()
 
 def alternar_estado_restaurante():
+    '''Essa função é responsável por alternar o estados dos restaurantes entre Ativo/Desativado
+    
+    Inputs:
+    - Nome do restaurente 
+
+    Output:
+    -Alterar o status do resturante selecionado
+
+    '''
     exibir_subtitulo('Alternando estado restaurante')
     nome_restaurante = input('Digite o nome do resturante que deseja alterar o estado: ')
     restaurante_encontrado = False
+    busca_restaurante = nome_restaurante.lower()
+    print(busca_restaurante)
 
     for restaurante in restaurantes:
-        if nome_restaurante == restaurante['nome']:
+        if busca_restaurante == restaurante['nome']:
             restaurante_encontrado = True
             restaurante['ativo'] = not restaurante['ativo']
             mensagem = f'O restaurante {nome_restaurante} foi ativo com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
